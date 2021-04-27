60fa99a0472f0569fee0d312e0a28adf67a99de4
Sree <vjsreekanth@gmail.com>
2021-04-27 10:38:44 -0700

    first commit post model created

---
+++ app/models/post.rb
@@ -0,0 +1,13 @@
+class Post < ApplicationRecord
+    has_many :comments, dependent: :destroy
+
+    validates(:title, presence: true, uniqueness: true)
+
+ 
+  validates(
+    :body, 
+    presence: { message: "must include a body" },
+    length: { minimum: 50 },
+  )
+
+end
